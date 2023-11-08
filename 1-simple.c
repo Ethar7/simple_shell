28944e9829ea7d23c63922548c460ed9c2b5f07f
Ethar7 <nabihethar@yahoo.com>
2023-11-08 07:21:03 -0800

    o

---
+++ 1-simple.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * cmpr - writes a prompt
+ *
+ * Return: 0
+ */
+int prompt(void)
+{
+	char *cmpr = "$ ";
+	ssize_t count = 0;
+
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		count = write(STDOUT_FILENO, cmpr, 2);
+	if (count == -1)
+		exit(0);
+	}
+	return (0);
+}
