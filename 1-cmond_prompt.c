05d9c4a13a6d852b5c2009d29d938fd8c6dbbbce
Ethar7 <nabihethar@yahoo.com>
2023-11-10 09:50:52 -0800

    p

---
+++ 1-cmond_prompt.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * prompt - writes a command prompt
+ *
+ * Return: 0
+ */
+int cmond_prompt(void)
+{
+	char *cmpr = "$ ";
+	ssize_t count = 0;
+
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		count = write(STDOUT_FILENO, cmpr, 2);
+	if (count == -1)
+		exit(0);
+	}
+	return (0);
+}
