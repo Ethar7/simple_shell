eea52c350ff0b537a2c7deb80cf4abd1f572b7ea
Ethar7 <nabihethar@yahoo.com>
2023-11-15 18:07:27 -0800

    o

--- main_simple.c
+++ main_simple.c
@@ -8,12 +8,35 @@
  */
 int main(void)
 {
-	char *c;
+	char *fullpath = NULL, *cp = NULL, *buf = NULL;
+	char *Path = NULL;
+	char **a;
+	int exitstate = 0;
 
-	while (1)
+	signal(SIGINT, SIG_IGN);
+	Path = _getenv("PATH");
+	if (path == NULL)
+		return (-1);
+	while (true)
 	{
-		c = NULL;
-		cmond_prompt();
+		a = NULL;
+		prompt();
+		buf = _read();
+		if (*buf != '\0')
+		{
+			a = tokenize(buf);
+			if (a == NULL)
+			{
+				free(buf);
+				continue;
+			}
+			fullpath = _fullpathbuffer(a, path, cp);
+			if (checkbuiltins(a, buf, exitstate) == 1)
+				continue;
+			exitstatus = _forkprocess(a, buf, fullpath);
+		}
+		else
+			free(buf);
 	}
 	return (0);
 }
