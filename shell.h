5f8a86d32c403ed514e5bf0536cfd39e5d236c54
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2023-11-16 04:19:04 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -16,4 +16,21 @@ extern char **envi;
 int cmond_prompt(void);
 char *read_stdin(void);
 int string_lenght(char *x);
+int _putchar(char x);
+char *stringtok(char *pointer, const char *deli);
+char *fullpath(char **a, char *path, char *cp);
+int checkbuiltinfun(char **a, char *buf, int status);
+int _forkpro(char **a, char *buf, char *fullpath);
+char *getenviron(const char *nam);
+int env(void);
+void putss(char *string);
+char *_mem(char *a, char x, unsigned int c);
+char *stringconcat(char *des, char *sr);
+int stringcmp(const char *str1, const char *str2);
+int stringcount(char *s);
+char *stringdupl(char *s);
+char **tokenarray(char *buf);
+
+int _pathcmp(const char *str1, const char *str2);
+char *concat(char *tmpo, char **a, char *token);
 #endif
